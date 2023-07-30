a52e2f67da1c501851d4d5dcd251f3fbfb6ff395
SahilDhillon21 <sodhillon_b21@it.vjti.ac.in>
2023-07-31 03:05:25 +0530

    Created news components and populated with fetch news api

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -7,7 +7,7 @@ export class NewsItem extends Component {
             <div>
 
                 <div className="card" style={{width: "18rem"}}>
-                    <img src={imageURL} className="card-img-top article-image" alt="..."/>
+                    <img src={imageURL?imageURL:"https://thumbs.dreamstime.com/b/news-newspapers-folded-stacked-word-wooden-block-puzzle-dice-concept-newspaper-media-press-release-42301371.jpg"} className="card-img-top article-image" alt="news-image"/>
                         <div className="card-body">
                             <h5 classNameName="card-title">{title}</h5>
                             <p className="card-text">{description}</p>
